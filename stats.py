3d1b94fc25b3e8245dc172dc1b63f451e60c1b3a
Aaron Miller <l3370x@gmail.com>
2014-01-29 12:59:52 -0700

    added plot

--- stats.py
+++ stats.py
@@ -1,6 +1,10 @@
 import os
 import xml.etree.ElementTree as ET
 from collections import Counter
+import operator
+import matplotlib
+import matplotlib.pyplot as plt
+import numpy as np
 
 DATADIR = "C:\\Users\\Aaron\\python\\nfl\\data"
 
@@ -44,3 +48,24 @@ for i in sorted(percentages,key=percentages.get, reverse=True):
 	print "Score: "+i+" - "+percentages[i]+"%"
 	percentagesFile.write("Score: "+i+" - "+percentages[i]+"%\n")
 	scoreCountFile.write("Score: "+i+" - "+str(scoreCount[i])+"\n")
+
+# fun plot
+normalized = [[0]*10 for x in xrange(10)]
+for s in percentages:
+	x = int(s[:1])
+	y = int(s[-1:])
+	normalized[x][y] = float(percentages[s])
+	normalized[y][x] = float(percentages[s])
+normA = np.array(normalized)
+fig, ax = plt.subplots()
+ax.set_xticks(np.arange(normA.shape[1]) + 0.5, minor=False)
+ax.set_yticks(np.arange(normA.shape[0]) + 0.5, minor=False)
+column_labels = list('0123456789')
+row_labels = list('0123456789')
+plt.pcolor(normA,cmap="Reds")
+plt.gca().invert_yaxis()
+plt.gca().xaxis.tick_top()
+ax.set_xticklabels(column_labels, minor=False)
+ax.set_yticklabels(row_labels, minor=False)
+plt.colorbar().set_label("Winning Percentages")
+plt.show()
